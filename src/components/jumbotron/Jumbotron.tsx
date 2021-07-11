97be09c1181a2f6e7eae668d2976a387ebf1576e
sanghmitravk <jaya13194@gmail.com>
2021-07-12 02:37:05 +0530

    updated video

--- src/components/jumbotron/Jumbotron.tsx
+++ src/components/jumbotron/Jumbotron.tsx
@@ -3,27 +3,14 @@ import Header from "../header/Header"
 import ImageRotate from "../imageRotate/ImageRotate";
 import desktopVideo from '../../images/JBN_Creative_Contet_Video.mp4';
 import tabletVideo from '../../images/JBN_Creative__Contet_Video_43.mp4';
+import mobileVideo from '../../images/JBN_Creative__Contet_Video_34.mp4';
+
 import './Jumbotron.scss';
 import { jumbtotron } from "../../config/SideWide";
 // import Slider from "../slider/Slider";
 
 const Jumbotron = () => {
-    // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    const useWindowWidth = () => {
-        const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-        useEffect(() => {
-            const handleWindowResize = () => {
-                setWindowWidth(window.innerWidth);
-            };
-
-            window.addEventListener('resize', handleWindowResize);
-            return () => typeof window !== undefined && window.removeEventListener('resize', handleWindowResize);
-        }, []);
-
-        return windowWidth;
-    };
-    const src = useWindowWidth() >= 1024 ? desktopVideo : tabletVideo;
 
     return (
         <div className="Jumbotron hero is-fullheight">
@@ -31,9 +18,15 @@ const Jumbotron = () => {
                 <Header />
             </div>
             <div className="hero-body p-0">
-                <video autoPlay loop muted>
-                    <source src={src} type="video/ogg" />
+                <video autoPlay loop muted className="is-hidden-touch">
+                    <source src={desktopVideo} type="video/ogg" />
                 </video>
+                <video autoPlay loop muted className="is-hidden-desktop">
+                    <source src={tabletVideo} type="video/ogg" />
+                </video>
+                {/* <video autoPlay loop muted className="">
+                    <source src={mobileVideo} type="video/ogg" />
+                </video>  */}
                 <div className="JumbotronText container">
                     <div className="columns ">
                         <div className="column JumbotronTextPadding is-one-third is-offset-half-desktop is-offset-three-fifths-tablet">
